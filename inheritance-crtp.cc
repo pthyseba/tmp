77e32241b1ddb3f8b2fcd7ca75b1b20c9ef758cb
Pieter Thysebaert <pieter.thysebaert@gmail.com>
2020-10-26 21:52:12 +0100

    Added data structure inheritance

--- inheritance-crtp.cc
+++ inheritance-crtp.cc
@@ -5,6 +5,9 @@ struct Base
 {
    typedef taMostDerived TUnderlying;
 
+   struct Data
+   {};
+
    TUnderlying& Top()
    {
       return static_cast<TUnderlying&>(*this);
@@ -12,22 +15,31 @@ struct Base
 
    void Process()
    {
-      Top().Action();
+      typedef typename TUnderlying::Data Data;
+      Data d;      
+      Top().Action(d);
    }
 
  protected:
-   void Action() {}
+   void Action(Data& aData) {}
 };
 
 
+// Functional layer examples
 template<typename taParent>
 struct Layer1 : public taParent
 {
   typedef taParent TSuper;
 
-  void Action()
+  struct Data : public TSuper::Data
   {
-    TSuper::Action();
+     int iElement1;
+  };
+
+  void Action(Data& aData)
+  {
+    TSuper::Action(aData);
+    aData.iElement1 = 1;
     std::cout << "Layer1::Action()" << std::endl;
   }
 };
@@ -36,17 +48,26 @@ template<typename taParent>
 struct Layer2 : public taParent
 {
   typedef taParent TSuper;	
-  void Action()
+  
+  struct Data : public TSuper::Data
   {
-     TSuper::Action();
+    int iElement2;
+  };
+
+  void Action(Data& aData)
+  {
+     TSuper::Action(aData);
+     aData.iElement2 = 2;
      std::cout << "Layer2::Action()" << std::endl;
   }	  
 };
 
+// Recursive helper class template
 template<typename taMostDerived, template<class...T> typename taTopLayer, template<class... T> typename... taOtherLayers>
 struct RecursiveLinearInherit : public taTopLayer<RecursiveLinearInherit<taMostDerived, taOtherLayers...>>
 {};
 
+// Recursive helper class template: base case
 template<typename taMostDerived, template<class...T> typename taTopLayer>
 struct RecursiveLinearInherit<taMostDerived, taTopLayer> : public taTopLayer<Base<taMostDerived>>
 {};
@@ -55,10 +76,18 @@ template<template<class...T> typename taTopLayer, template<class... T> typename.
 struct LinearInherit : public taTopLayer<RecursiveLinearInherit<LinearInherit<taTopLayer, taOtherLayers...>, taOtherLayers...> >
 {};
 
+// Specialization for single layer
+template<template<class...T> typename taTopLayer>
+struct LinearInherit<taTopLayer> : public taTopLayer<Base<LinearInherit<taTopLayer>>>
+{};
+
 int main(int argc, char** argv)
 {
-  LinearInherit<Layer2,Layer1> l;
-  l.Process();
+  typedef LinearInherit<Layer2, Layer1> TProduct;
+  TProduct p;
+	
+  p.Process();
+  
   return 0;
 }
 
